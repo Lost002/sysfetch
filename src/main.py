4b4f06f0255bc24e944d7d00f172f1ba2fb8df2d
Lost002 <144188945+Lost002@users.noreply.github.com>
2023-09-26 16:09:25 +0000

    Got RAM percentage

--- src/main.py
+++ src/main.py
@@ -5,12 +5,14 @@ class main:
         self.os = popen('cat /etc/os-release').read().replace("NAME=", "").replace("\"", "")
         self.os = self.os[:self.os.index("VERSION")].replace("\n", "")
         self.ram = str(psutil.virtual_memory())[6:]
-        self.ram = self.ram[:self.ram.index(",")].replace("total=", "")
-        self.ram = int(self.ram)/1_000_000_000
-        self.ram = round(self.ram)
-        self.ram = "RAM: total= " + str(self.ram) + " GB"
+        self.total = self.ram[:self.ram.index(",")].replace("total=", "")
+        self.total = int(self.total)/1_000_000_000
+        self.total = round(self.total)
+        self.percent = self.ram[self.ram.index("percent"):].replace("percent=", "")
+        self.percent = self.percent[:self.percent.index(",")]
+        self.ram = "RAM: Total= " + str(self.total) + " GB - Percent: " + self.percent
 
-        self.fetch = self.os+" | "+self.ram
+        self.fetch = self.os + " | "+ self.ram
     def __str__(self):
         return self.fetch
 print(main())
