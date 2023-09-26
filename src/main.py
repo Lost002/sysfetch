5c093c77c8b1be5dd5976463f49af8f976f78e06
Lost002 <144188945+Lost002@users.noreply.github.com>
2023-09-26 18:20:40 +0000

    modified:   src/main.py

--- src/main.py
+++ src/main.py
@@ -7,12 +7,16 @@ class main:
         self.ram = str(psutil.virtual_memory())[6:]
         self.total = self.ram[:self.ram.index(",")].replace("total=", "")
         self.total = int(self.total)/1_000_000_000
-        self.total = round(self.total)
+        self.total = round(self.total,1)
         self.percent = self.ram[self.ram.index("percent"):].replace("percent=", "")
         self.percent = self.percent[:self.percent.index(",")]
-        self.ram = "RAM: Total= " + str(self.total) + " GB - Percent: " + self.percent
+        self.in_use = self.ram[self.ram.index("used"):].replace("used=", "")
+        self.in_use = self.in_use[:self.in_use.index(",")]
+        self.in_use = int(self.in_use)/1_000_000_000
+        self.in_use = round(self.in_use,1)
+        self.ram = "RAM: Total: " + str(self.total) + " GB - Percent: " + self.percent + "% - Used: " + str(self.in_use) + "GB"
 
-        self.fetch = self.os + " | "+ self.ram
+        self.fetch = "Distro: " + self.os + " | "+ self.ram + "\n\n" + str(psutil.virtual_memory())
     def __str__(self):
         return self.fetch
 print(main())
