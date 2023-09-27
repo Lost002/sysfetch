172cf0c9e3d1f25e3baaf4ebf934c193110f4a6d
Lost002 <144188945+Lost002@users.noreply.github.com>
2023-09-27 14:48:11 -0500

    Fixed make.sh

--- beta0.1/make/make.sh
+++ beta0.1/make/make.sh
@@ -1,2 +1,2 @@
-sudo mkdir /usr/bin/SysFetch && sudo mkdir /usr/bin/SysFetch/python && sudo cp ../../src/main.py /usr/bin/SysFetch/python
+sudo mkdir /usr/bin/SysFetch && sudo mkdir /usr/bin/SysFetch/python && sudo cp ../python/main.py /usr/bin/SysFetch/python
 alias sysfetch="python3 /usr/bin/SysFetch/python/main.py"
