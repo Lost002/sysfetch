cf1337117c009e1a47e800f7cf332cd0dabefcac
Lost002 <144188945+Lost002@users.noreply.github.com>
2023-10-02 14:30:48 +0000

    modifed: parseconfig.py

--- src/parseconfig.py
+++ src/parseconfig.py
@@ -7,7 +7,7 @@ class getcolor:
         "red": "31",
         "green": "32",
         "yellow": "33",
-        "blue":"34",
+        "blue": "34",
         "magenta": "35",
         "cyan": "36",
         "white": "37",
