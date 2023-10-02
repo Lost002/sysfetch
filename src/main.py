f8c35eacf4a1cf81502e269c3bd29b75e50eb75e
Lost002 <connerstanek18@gmail.com>
2023-10-01 21:04:51 -0500

    Added free ram status, started working on config file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from os import popen
+from parseconfig import getcolor
 import psutil
 
 RAM:str = str(psutil.virtual_memory())[6:]
@@ -7,28 +8,34 @@ BYTES_TO_GB:int = 1_000_000_000
 class main:
     def __init__(self):
         self.SEPERATOR:str = "\033[31m\n|\033[0m "
+        #self.SEPERATOR:str = getcolor.fg_color_text+"\n|"+getcolor.reset
         self.OS_SEPERATE:str = ""
         self.TOTAL_SEPERATE:str = ""
         self.PERCENT_SEPERATE:str = ""
         self.IN_USE_SEPERATE:str = ""
         self.TOP_BOTTOM:str = ""
+        self.FREE_SEPERATE:str = ""
 
-        for i in range(max(len(os), len(total), len(percent), len(in_use))-len(os)):
+        for i in range(max(len(os), len(total), len(percent), len(in_use), len(free))-len(os)):
             self.OS_SEPERATE += " "
-        for i in range(max(len(os), len(total), len(percent), len(in_use))-len(total)):
+        for i in range(max(len(os), len(total), len(percent), len(in_use), len(free))-len(total)):
             self.TOTAL_SEPERATE += " "
-        for i in range(max(len(os), len(total), len(percent), len(in_use))-len(percent)):
+        for i in range(max(len(os), len(total), len(percent), len(in_use), len(free))-len(percent)):
             self.PERCENT_SEPERATE += " "
-        for i in range(max(len(os), len(total), len(percent), len(in_use))-len(in_use)):
+        for i in range(max(len(os), len(total), len(percent), len(in_use), len(free))-len(in_use)):
             self.IN_USE_SEPERATE += " "
-        for i in range(max(len(os), len(total), len(percent), len(in_use))+4):
+        for i in range(max(len(os), len(total), len(percent), len(in_use), len(free))-len(in_use)):
+            self.FREE_SEPERATE += " "
+        for i in range(max(len(os), len(total), len(percent), len(in_use), len(free))+4):
             self.TOP_BOTTOM += "-"
+
         self.TOP_BOTTOM += "\n"
         self.OS_SEPERATE += "\033[31m |\033[0m"
         self.TOTAL_SEPERATE += "\033[31m |\033[0m"
         self.PERCENT_SEPERATE += "\033[31m |\033[0m"
         self.IN_USE_SEPERATE += "\033[31m |\033[0m"
-        self.fetch = f"\033[31m{self.TOP_BOTTOM}|\033[0m {os}{self.OS_SEPERATE}{self.SEPERATOR}{total}{self.TOTAL_SEPERATE}{self.SEPERATOR}{percent}{self.PERCENT_SEPERATE}{self.SEPERATOR}{in_use}{self.IN_USE_SEPERATE}\033[31m\n{self.TOP_BOTTOM}\033[0m"
+        self.FREE_SEPERATE += "\033[31m |\033[0m"
+        self.fetch = f"\033[31m{self.TOP_BOTTOM}|\033[0m {os}{self.OS_SEPERATE}{self.SEPERATOR}{total}{self.TOTAL_SEPERATE}{self.SEPERATOR}{percent}{self.PERCENT_SEPERATE}{self.SEPERATOR}{in_use}{self.IN_USE_SEPERATE}{self.SEPERATOR}{free}{self.FREE_SEPERATE}\033[31m\n{self.TOP_BOTTOM}\033[0m"
     
     def os():
         """Get the Linux Distro of user's system"""
@@ -63,11 +70,20 @@ class main:
         in_use = round(in_use,2)
         in_use = f"RAM: In Use:  {str(in_use)} GB"
         return in_use
+    def RAM_free():
+        global free
+        free = RAM[RAM.index('free'):].replace("free=", "")
+        free = free[:free.index(",")]
+        free = int(free)/BYTES_TO_GB
+        free = round(free, 2)
+        free = f"RAM: Free:    {str(free)} GB"
+        return free
 
     os = os()
     total = RAM_total()
     percent = RAM_percent()
     in_use = RAM_in_use()
+    free = RAM_free()
 
     def __str__(self):
         return self.fetch
---
+++ src/parseconfig.py
@@ -0,0 +1,33 @@
+import json
+class getcolor:
+    with open('.config/.config.jsonc') as conf:
+        config = json.load(conf)
+    fg = {
+        "black": "30",
+        "red": "31",
+        "green": "32",
+        "yellow": "33",
+        "blue":"34",
+        "magenta": "35",
+        "cyan": "36",
+        "white": "37",
+        "0": "0",
+        "1": "1"
+    }
+    bg = {
+        "black": "40",
+        "red": "41",
+        "green": "42",
+        "yellow": "43",
+        "blue":"44",
+        "magenta": "45",
+        "cyan": "46",
+        "white": "47",
+        "0": "0",
+        "1": "1"
+    }
+    fg_color_border = "\033[0;"+fg[config["fg-color-border"]]+"m".replace(" ", "")
+    fg_color_text = "\033[0;"+fg[config["fg-color-text"]]+"m".replace(" ", "")
+    bg_color_border = "\033[0;"+fg[config["bg-color-border"]]+"m".replace(" ", "")
+    bg_color_text = "\033[0;"+fg[config["bg-color-text"]]+"m".replace(" ", "")
+    reset = "\033[0m".replace(" ", "")
